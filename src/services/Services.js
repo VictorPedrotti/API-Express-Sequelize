383254d3c6037758aff29a86f8501af5fcbe59f3
Victor Pedrotti <victor1404pedrotti@gmail.com>
2024-04-30 22:13:43 -0300

    Criando transação ao desativar estudante

--- src/services/Services.js
+++ src/services/Services.js
@@ -29,13 +29,15 @@ class Services {
     return dataSource[this.model].create(dadosDoRegistro);
   }
 
-  async atualizaRegistro(dadosAtualizados, where) {
-    const ListaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
-      where: { ...where }
-    });
-    if (ListaDeRegistrosAtualizados[0] === 0) {
+  async atualizaRegistro(dadosAtualizados, where, transacao = {}) {
+    const listadeRegistrosAtualizados = await dataSource[this.model]
+      .update(dadosAtualizados, {
+        where: { ...where },
+        transaction: transacao
+      });
+    if (listadeRegistrosAtualizados[0] === 0) {
       return false;
-    } 
+    }
     return true;
   }
 
